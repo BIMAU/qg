1a12fdc83a4a6cc680a8ae8d44ec009a6eff6446
Erik <t.e.mulder@uu.nl>
2020-02-26 16:14:05 +0100

    restructuring of test_periodic

--- matlab/MLQG_tests/test_periodic.m
+++ matlab/MLQG_tests/test_periodic.m
@@ -1,72 +1,20 @@
 % Initialize QG
-nx = 256;
-ny = 256;
+nx = 128;
+ny = 128;
 n  = nx * ny * 2;
 qg = QG(nx, ny, 1);
 
-% qg.set_par(11, 0.1);  % wind stress
-                        % qg.set_par(5, 45);    % Reynolds number
-
-% load testing data
-% testdata = load('testdata.mat');
-% xr = testdata.xr(1:n);
-
-%% Test 1: analytical jacobian vs numerical jacobian
-
-% J  = -qg.jacobian(xr, 0.0);
-% 
-% F0 = qg.rhs(xr);
-% 
-% pert = 1e-6;
-% err  = zeros(n,1);
-% D    = zeros(n,n);
-% Jn   = zeros(n,n);
-% 
-% v = zeros(n,1);
-% for i = 1:n
-    %     v(i) = pert;
-    %     Jn(:,i) = (qg.rhs(xr+v) - qg.rhs(xr-v)) / (2*pert);
-    %     v(i) = 0;
-    %     
-    %     D(:,i)  = abs(Jn(:,i)  - J(:,i));
-    %     err(i)  = max(D(:,i));
-    % end
-    %
-
-%%  --> check dit in relatieve zin...
-% Jn = sparse(Jn);
-% D(abs(D) < 1) = 0.0;
-% D = sparse(D);
-% 
-% assert(max(err) < 1e-4);
-
-%vsm(J)
-%vsm(Jn-J)
-
-%% Test 2: perform a few backward Euler time steps
-
-% Edeling/Verkley params:
-nu  = 1.79957939712e-05;
-mu  = 0.00176358780918;
-L   = 6.371e6;
-T   = 1/(7.292e-5);
-nud = nu * L^2 / T
-mud = mu / T
-
 % Henk QG params:
 Ldim = 1e6;
-Udim = 1.6e-2;
+Udim = 3.171e-2;
 tdim = Ldim / Udim; % in seconds
+day  = 3600 * 24 / tdim;
+year = 365*day;
 
-day = 3600 * 24 / tdim;
+Re   = 10000;
+wind = 0;
 
-Ah = 1.8e-05;
-Ldim*Udim/Ah
-Re   = 5000;
-wind = 2;
-
-% qg.set_par(1, 1);    % alpha_tau
-qg.set_par(11, wind);  % enable  wind stress
+qg.set_par(11, wind);  % wind stress (stirring) amplitude
 qg.set_par(5,    Re);  % Reynolds number
 qg.set_par(2,   0.0);  % no rotation
 
@@ -107,22 +55,21 @@ t = 0;
 states = [];
 times = [];
 storeTime = 0;
-days = 600;
+Tend = Re;     % e-folding timescale largest mode (?)
+
 tic
-while t < days*day
-    fprintf(' t = %2.2e days,  \n',  t / day);
+while t < Tend
+    fprintf(' t = %2.2e years,  \n',  t / year);
     fprintf('dt = %2.2e days \n Newton: \n', dt / day);
-    fprintf('start Newton\n')
+    fprintf('start Newton \n')
 
     for k = 1:10
-
         rhs = B*(x-x0)/(dt*th) + F(x) + (1-th)/th * F0;
         J   = qg.jacobian(x, s);
         dx  = J \ rhs;
         x   = x + dx;
-
-        fprintf('||dx|| = %2.5e\n', norm(dx));
-        if norm(dx,2) < 1e-5
+        if norm(dx,2) < 1e-3
+            fprintf('||dx|| = %2.5e\n', norm(dx));
             break;
         end
     end   
@@ -130,32 +77,37 @@ while t < days*day
     dt = kDes / k * dt;
     s  = 1.0 / (dt*th);
          
-    %assert(norm(dx,2) < 1e-7);
-    
     x0 = x;
     F0 = F(x);
 
-    if t > storeTime || t > days*day
+    if t > storeTime || t > Tend
         states = [states, x];
         times  = [times,  t];
-
+        
         subplot(1,2,1);
-        plotQG(nx,ny,2,x)
-        titleString = sprintf('t = %f days', t / day);
+        plotQG(nx,ny,2,x);
+        titleString = sprintf('t = %f year', t / year);
         title(titleString);
-        %        exportfig(['psi_Re',num2str(Re),'.eps'])
-
+        
         subplot(1,2,2);
-        plotQG(nx,ny,1,3600*24/tdim*x,false)
-        titleString = sprintf('t = %f days', t / day);
+        plotQG(nx,ny,1,3600*24/tdim*x,false);
+        titleString = sprintf('t = %f year', t / year);
         title(titleString);
-        %        exportfig(['zeta_Re',num2str(Re),'.eps'])
+        
+        fnamebase = ['N',num2str(nx), '_Re', num2str(Re), '_Tend', ...
+                 num2str(Tend), '_wind', num2str(wind)];
+        
+        exportfig([fnamebase,'.eps'],10,[30,10]);
 
-        storeTime = t + 10*day;
-        drawnow
+        fprintf('saving data to %s\n', [fnamebase,'.mat']);
+        
+        save([fnamebase,'.mat'], 'states', 'times', 'nx', 'ny', 'Re', ...
+             't', 'dt', 'wind');
+        
+
+        storeTime = t + 0.1*year;
     end
 end
 toc
+toc / 3600
 
-save(['N',num2str(nx),'_Re',num2str(Re),'_days',num2str(days),'.mat']...
-     ,'states','times','nx','ny','Re','t','dt','wind');
