397d7701e118afa405b51eb330e65061a927df1a
Erik <t.e.mulder@uu.nl>
2020-10-22 10:48:33 +0200

    factorized all actions for a wavelet basis

---
+++ matlab/MLQG/create_wavelet_basis.m
@@ -0,0 +1,29 @@
+function [H_out] = create_wavelet_basis(nx, ny, nun, bs, nested)
+% Create wavelet basis for a 2D grid with nested block decompositions
+% #TODO make nesting level optional
+    
+    time = tic;
+    fprintf('Create wavelet basis... \n');
+    
+    dim = nx*ny*nun;
+    
+    [all, P] = blockpermutation(nx,ny,nun,bs,nested);
+    
+    H = haarmat(bs^2);
+    
+    % create a block diagonal matrix with dim/(bs^2) blocks
+    M = speye(dim / (bs^2)); 
+    H = kron(M, H);
+
+    % create row ordering: orders the average and difference modes from
+    % coarse to fine
+    T  = speye(dim);
+    id = [];
+    for i = 1:bs^2
+        id = [id, (i:bs^2:dim)];
+    end
+    T(:,id) = T(:,1:dim);
+
+    H_out = T*H*P;
+    fprintf('Create wavelet basis... done (%f)\n', toc(time));        
+end
