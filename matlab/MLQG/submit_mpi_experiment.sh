d02f2913d395eef0391d15c306469165cfbc4d1a
Erik@Cartesius <t.e.mulder@rug.nl>
2020-12-10 17:58:59 +0100

    less nodes = less data

--- matlab/MLQG/submit_mpi_experiment.sh
+++ matlab/MLQG/submit_mpi_experiment.sh
@@ -2,8 +2,9 @@
 
 #SBATCH --time=02:00:00
 #SBATCH --nodes=1
-#SBATCH --tasks=32
-#SBATCH -p fat
+#SBATCH --tasks=8
+#SBATCH --tasks-per-node=8
+#SBATCH -p normal
 
 module load MCR/R2018a
 export MCR_CACHE_ROOT=`mktemp -d /scratch-local/mcr.XXXXXX`
@@ -13,4 +14,4 @@ cd /home/emulder/Projects/qg/matlab/MLQG
 # mkdir -p data/experiments/logdir
 # rm data/experiments/logdir/*
 
-srun -n 32 ./mpi_experiment
+srun -n 8 ./mpi_experiment
