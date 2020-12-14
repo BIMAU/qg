a9166e602bd30a3e8c96aeabcf3a1b47e760ee9c
Erik <t.e.mulder@rug.nl>
2020-12-14 13:25:21 +0100

    expanded stopping criteria

--- matlab/MLQG/qg_stopping_criterion.m
+++ matlab/MLQG/qg_stopping_criterion.m
@@ -1,20 +1,29 @@
 function [stopFlag, err, testSpec, predSpec] = qg_stopping_criterion(qg, predY, testY)
+    use_fields   = false;
+    use_spectrum = true;
+    err_tol = 4.0;
+    
     Ldim    = 1e6;
     Udim    = 3.171e-2;
     tdim    = Ldim / Udim; % in seconds
     scaling = 3600*24/tdim;    
     
-    testSpec = computeQGspectrum(qg, scaling*testY);
-    predSpec = computeQGspectrum(qg, scaling*predY);
+    if use_fields
+        diff = abs(scaling*(testY-predY));
+        err  = norm(diff(:),2) / norm(scaling*testY(:), 2);
+    
+    elseif use_spectrum
+        testSpec = computeQGspectrum(qg, scaling*testY);
+        predSpec = computeQGspectrum(qg, scaling*predY);
 
-    sd   = find(testSpec > 1e-10);
-    testSpec = testSpec(sd);
-    predSpec = predSpec(sd);
-    diff = (log(testSpec) - log(predSpec));
-    err  = norm(diff,2);
+        sd   = find(testSpec > 1e-10);
+        testSpec = testSpec(sd);
+        predSpec = predSpec(sd);
+        diff = (log(testSpec) - log(predSpec));
+        err  = norm(diff,2);
+    end
 
-    % ARBITRARY, make this more informed #FIXME
-    if err > 4
+    if err > err_tol
         stopFlag = true;
     else
         stopFlag = false;
